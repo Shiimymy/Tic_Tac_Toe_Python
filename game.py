f313fe074b340e804823dd61313f139c67589bfa
Eugénie <eugenie.b24@hotmail.fr>
2024-09-25 16:10:31 +0100

    Add win_check function

--- game.py
+++ game.py
@@ -47,6 +47,21 @@ place_marker(test_board,'$',8)
 display_board(test_board)
 
 
+def win_check(board,mark):
+    
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
+display_board(test_board)
+win_check(test_board,'X')
+
+
 def choose_first():
     '''
     Randomly decide which player goes first
@@ -98,3 +113,5 @@ def replay():
     Ask the player if they want to play again
     '''
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+
